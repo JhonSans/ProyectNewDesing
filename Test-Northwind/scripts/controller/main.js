acd0b477fdfb85d88cbf07120842f5ce4986c5fd
Jhonatan Santanila C <jstevensantani@gmail.com>
2023-09-29 13:54:32 -0500

    Actualización NAVBAR

--- Test-Northwind/scripts/controller/main.js
+++ Test-Northwind/scripts/controller/main.js
@@ -1,11 +1,37 @@
-mainApp.controller("mainController", function ($scope, $timeout) {
-    $scope.headerExpand = true;
-    $scope.statusEye = true;
-    $scope.imgMove = true;
-    $scope.imgNext = null;
-    $scope.imgNext2 = null;
-    $scope.imgNext3 = null;
+mainApp.controller("mainController", function ($scope) {
+
+    // $scope.headerExpand = true;
+    // $scope.statusEye = true;
+    // $scope.imgMove = true;
+    // $scope.imgNext = null;
+    // $scope.imgNext2 = null;
+    // $scope.imgNext3 = null;
+
+    // $scope.config = {
+    //     startSlide: 0,
+    //     speed: 0,
+    //     auto: 2000,
+    //     draggable: true,
+    //     continuous: true,
+    //     autoRestart: true,
+    //     disableScroll: false,
+    //     stopPropagation: false,
+    //     callback: function(index, elem) {},
+    //     transitionEnd: function(index, elem) {}
+    // }
+
+    // $scope.seleccionar = function (item) {
+    //     $scope.imgNext = !$scope.imgNext ? angular.copy(item) : null;
+    // }
+    // $scope.seleccionar2 = function (item) {
+    //     $scope.imgNext2 = !$scope.imgNext2 ? angular.copy(item) : null;
+    // }
+    // $scope.seleccionar3 = function (item) {
+    //     $scope.imgNext3 = !$scope.imgNext3 ? angular.copy(item) : null;
+    // }
+
     $scope.toggleMenu = "Default";
+    $scope.toggle = false;
 
     $scope.news = [
         { Picture: "content/back-1.jpg", Name: "First" },
@@ -14,34 +40,16 @@ mainApp.controller("mainController", function ($scope, $timeout) {
         { Picture: "content/back-4.jpg", Name: "Fourth" }
     ];
 
-    $scope.config = {
-        startSlide: 0,
-        speed: 0,
-        auto: 2000,
-        draggable: true,
-        continuous: true,
-        autoRestart: true,
-        disableScroll: false,
-        stopPropagation: false,
-        callback: function(index, elem) {},
-        transitionEnd: function(index, elem) {}
-    }
-
-    $scope.seleccionar = function (item) {
-        $scope.imgNext = !$scope.imgNext ? angular.copy(item) : null;
-    }
-    $scope.seleccionar2 = function (item) {
-        $scope.imgNext2 = !$scope.imgNext2 ? angular.copy(item) : null;
-    }
-    $scope.seleccionar3 = function (item) {
-        $scope.imgNext3 = !$scope.imgNext3 ? angular.copy(item) : null;
-    }
-
     $scope.toggleMenuAction = function () {
-        if ($scope.toggleMenu == "Default")
+
+        if ($scope.toggleMenu == "Default") {
             $scope.toggleMenu = "In"
-        else if ($scope.toggleMenu == "In")
+            $scope.toggle = !$scope.toggle
+        }
+        else if ($scope.toggleMenu == "In") {
             $scope.toggleMenu = "Down"
+            $scope.toggle = !$scope.toggle
+        }
         else {
             $scope.toggleMenu = "Default"
             $scope.toggleMenuAction();
